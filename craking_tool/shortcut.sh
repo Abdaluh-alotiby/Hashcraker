813edb401f44d546df3b80440f66ec9c4c9574fd
Abdaluh-alotiby <116024722+Abdaluh-alotiby@users.noreply.github.com>
2024-01-18 22:00:32 +0300

    Create shortcut.sh

---
+++ craking_tool/shortcut.sh
@@ -0,0 +1,18 @@
+#!/bin/ash
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+current_directory=$(pwd)
+echo "Current working directory: $current_directory"
+
+if [ -e "main.py" ]; then
+    mv main.py /bin/main.py
+    echo "File successfully moved to /bin/"
+else
+    echo -e "${RED}Failed file [ main.py] does not exist.${NC}"
+fi
+alias hashcraker='python3 /bin/main.py'
+echo 'you can run script useing hashcraker'
